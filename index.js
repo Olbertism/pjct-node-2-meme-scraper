f512b7a8bffae54edb32f1c0ff73b8f83a22b71f
Albert Pichler <albertpichler@gmx.net>
2022-05-05 14:28:56 +0200

    Update to custom meme generator

--- index.js
+++ index.js
@@ -35,9 +35,41 @@ function createOutputFolder() {
   return;
 }
 
-function saveIMG(adressList) {
-  createOutputFolder();
+function requestAndSaveImage(reqOptions) {
+  https
+    .request(reqOptions, (response) => {
+      const chunks = [];
 
+      console.log(
+        'Received response with Status Code ' +
+          response.statusCode +
+          ' for picture ' +
+          reqOptions.outputPath.slice(8),
+      );
+
+      response.on('error', (error) => {
+        console.log(error);
+      });
+
+      response.on('data', (chunk) => {
+        chunks.push(chunk);
+      });
+
+      response.on('end', () => {
+        const buffer = Buffer.concat(chunks);
+        fs.writeFile(reqOptions.outputPath, buffer, (error) => {
+          if (error) {
+            throw error;
+          } else {
+            console.log('Saving picture ' + reqOptions.outputPath.slice(8));
+          }
+        });
+      });
+    })
+    .end();
+}
+
+function batchRequestPics(adressList) {
   let count = 1;
 
   for (const img of adressList) {
@@ -49,49 +81,70 @@ function saveIMG(adressList) {
       method: 'GET',
       outputPath: './memes/0' + count + '.jpg',
     };
-
-    https
-      .request(imgOptions, (response) => {
-        const chunks = [];
-
-        console.log(
-          'Received response with Status Code ' +
-            response.statusCode +
-            ' for picture ' +
-            imgOptions.outputPath.slice(8),
-        );
-
-        response.on('error', (error) => {
-          console.log(error);
-        });
-
-        response.on('data', (chunk) => {
-          chunks.push(chunk);
-        });
-
-        response.on('end', () => {
-          const buffer = Buffer.concat(chunks);
-          fs.writeFile(imgOptions.outputPath, buffer, (error) => {
-            if (error) {
-              throw error;
-            } else {
-              console.log('Saving picture ' + imgOptions.outputPath.slice(8));
-            }
-          });
-        });
-      })
-      .end();
+    requestAndSaveImage(imgOptions);
     count++;
   }
 }
 
+// text param needs to be an array with strings
 function generateMeme(templates, id, text) {
   if (templates.some((entry) => entry.id === id)) {
     console.log('Meme template exists....');
+    const requestBody = JSON.stringify({
+      style: ['string'],
+      text: text,
+      font: 'string',
+      extension: 'string',
+      redirect: true,
+    });
+    const url = new URL('https://api.memegen.link/templates/' + id);
+    const generatorOptions = {
+      host: url.host,
+      path: url.pathname,
+      protocol: url.protocol,
+      request: requestBody,
+      method: 'POST',
+    };
+
+    const request = https.request(generatorOptions, (response) => {
+      response.on('data', (chunk) => {
+        // This part still confuses me...
+        process.stdout.write(chunk);
+      });
+
+      response.on('end', () => {
+        console.log('Requesting download from: ' + response.headers.location);
+        const apiUrl = new URL(response.headers.location);
+        const imgOptions = {
+          host: apiUrl.host,
+          protocol: apiUrl.protocol,
+          path: apiUrl.pathname,
+          method: 'GET',
+          outputPath: './memes/custom.jpg',
+        };
+        requestAndSaveImage(imgOptions);
+      });
+    });
+
+    request.write(generatorOptions.request);
+
+    request.end();
   } else {
     console.log('Meme template does not exist, aborting...');
     return;
   }
+}
+
+function parseCLIText() {
+  let upperText = '';
+  let lowerText = '';
+  if (process.argv[3]) {
+    upperText = process.argv[3].split('-').join(' ');
+  }
+  if (process.argv[4]) {
+    lowerText = process.argv[4].split('-').join(' ');
+  }
+  return [upperText, lowerText];
 }
 
 if (process.argv[2]) {
@@ -107,7 +160,8 @@ if (process.argv[2]) {
 
       response.on('end', () => {
         const templates = JSON.parse(rawData);
-        generateMeme(templates, process.argv[2], process.argv[3]);
+        createOutputFolder();
+        generateMeme(templates, process.argv[2], parseCLIText());
       });
     })
     .end();
@@ -123,7 +177,8 @@ if (process.argv[2]) {
       response.on('end', () => {
         const dom = setupDOM(rawData);
         const imgAdresses = filterIMGAdressArray(dom, options.imgAmount);
-        saveIMG(imgAdresses);
+        createOutputFolder();
+        batchRequestPics(imgAdresses);
       });
     })
     .end();
